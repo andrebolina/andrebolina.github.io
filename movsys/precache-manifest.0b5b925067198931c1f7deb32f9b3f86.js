652cc2b1199ec382d0866d2556b58152bb4f9bf7
andrebolina <bolina.ac@gmail.com>
2020-08-08 14:42:05 -0300

    update movsys

---
+++ movsys/precache-manifest.0b5b925067198931c1f7deb32f9b3f86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e716998a3fa39ebb5363eec8865c56c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c3e190d422b0580a3a4",
+    "url": "./static/js/2.34c748b5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.34c748b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75a574be3ec6f235029",
+    "url": "./static/js/main.a5217a3c.chunk.js"
+  },
+  {
+    "revision": "aa28afea7879bc2f6833",
+    "url": "./static/js/runtime-main.604e9d23.js"
+  },
+  {
+    "revision": "3c03788e7ee28186a88979c525adfb3f",
+    "url": "./static/media/logo.3c03788e.png"
+  }
+]);
