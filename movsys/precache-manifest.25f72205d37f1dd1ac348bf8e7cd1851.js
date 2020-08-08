751a39155ac8c15f3db95bfeea27c35d95a1990f
andrebolina <bolina.ac@gmail.com>
2020-08-08 05:54:54 -0300

    add new dist

---
+++ movsys/precache-manifest.25f72205d37f1dd1ac348bf8e7cd1851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb53252591a952cb1185a8d49898ab0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1b140dc8fde029cdede",
+    "url": "./static/js/2.40b8eff3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.40b8eff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464c66f399f3b5b96947",
+    "url": "./static/js/main.62eca1d3.chunk.js"
+  },
+  {
+    "revision": "aa28afea7879bc2f6833",
+    "url": "./static/js/runtime-main.604e9d23.js"
+  }
+]);
